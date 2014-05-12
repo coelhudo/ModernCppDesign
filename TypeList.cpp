1e765fb6c44d78338cbd56304faf9e2c99323e22
coelhudo <coelhudo@gmail.com>
2014-05-12 10:39:49 -0300

    Adding IndexOf for searching an element

--- TypeList.cpp
+++ TypeList.cpp
@@ -34,13 +34,19 @@ template<class T, class U, unsigned int index> struct TypeAt<TypeList<T,U>,index
    typedef typename TypeAt<U,index-1>::Result Result;
 };
 
+template<class TList, class T> struct IndexOf;
+template<class T> struct IndexOf<NullType, T>
+{
+   enum { value = -1 };
+};
+
 int main()
 {
    typedef TypeList<unsigned long, TypeList<std::string,NullType> > TypeListT;
    std::cout << Length<TypeListT>::value << "\n";
    std::cout << typeid(typename TypeAt<TypeListT,0>::Result).name() << "\n";
    std::cout << typeid(typename TypeAt<TypeListT,1>::Result).name() << "\n";
-   std::cout << typeid(typename TypeAt<TypeListT,2>::Result).name() << "\n";
+   std::cout << IndexOf<NullType, NullType>::value << "\n";
 
    return 0;
 }
